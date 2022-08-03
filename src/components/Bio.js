b0b2d2e805e8ae86b5813b8bba13d247a9087780
tjlaud <62766674+tjlaud@users.noreply.github.com>
2022-08-03 15:53:08 +0100

    Al1
    nearly finished portfolio

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,25 +1,36 @@
 import React from "react";
 import "../App.css";
+import profilePic from "../assets/profile.png";
+import htmlLogo from "../assets/logos/html.png";
+import cssLogo from "../assets/logos/css.png";
+import jsLogo from "../assets/logos/javascript.jpg";
+import sassLogo from "../assets/logos/sass.png";
+
+import { FaReact } from "react-icons/fa";
 
 function Bio() {
   return (
     <div className="bio-container">
-      <div>About me</div>
-      <div className="photo"></div>
-      <div>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-        ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-        quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-        arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-        Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras
-        dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend
-        tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus.
-        Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-        imperdiet. Etiam ultricies nisi vel augue.
+      <h2>Thomas J Laud</h2>
+      <div classname="profile-pic">
+        <img src={profilePic} alt="profile" />
       </div>
+
+      <p>
+        I'm Thomas Laud and I'm a front-end developer living in Hampshire. I
+        have served in the military as a Weapons Systems Operator on the Chinook
+        helicopter and after nearly 15 years of operational service I made the,
+        quite significant, career change into web development. <br></br>
+        <br></br> I have experience working with the following languages,
+        frameworks and libraries:
+      </p>
+      <aside className="logo-bar">
+        <img src={htmlLogo} alt="html logo" />
+        <img src={cssLogo} alt="css logo" />
+        <img src={jsLogo} alt="javascript logo" />
+        <FaReact className="logo"></FaReact>
+        <img src={sassLogo} alt="sass logo" />
+      </aside>
     </div>
   );
 }
